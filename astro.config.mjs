35289e027495bf5330a6c7adc73c8a8ff9361af9
PolarisNox <fufbgvhhyfjghccufvhhh8@gmail.com>
2026-04-29 17:29:50 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://polarisnox.github.io/",
+	site: "https://polarisnox.nacocx.top/",
 	base: "/myBlog",
 	trailingSlash: "always",
 	integrations: [
